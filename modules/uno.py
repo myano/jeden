0f0e90aeec41726a67be80600f37eb256a221cf8
Michael Yanovich <yanovich.1@osu.edu>
2010-07-16 16:35:57 -0400

    added the 2 classes to uno.py

---
+++ modules/uno.py
@@ -0,0 +1,16 @@
+"""
+uno.py - Computer Playing Bot
+"""
+
+import random
+
+class uno:
+    def __init__ (self):
+        self.game_on = False
+        self.current_cards = { }
+
+    def
+
+
+def game_start(phenny, input):
+

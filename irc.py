a9d0af95659e9cd8c2f0887a571f49c8f210dae3
Michael Yanovich <yanovich.1@osu.edu>
2010-07-16 12:17:11 -0400

    Updated gitignore to ignore compiled versions of the files. Also added uno.py

--- irc.py
+++ irc.py
@@ -141,14 +141,12 @@ class Bot(asynchat.async_chat):
 					time.sleep(wait - elapsed)
 
 		# Loop detection
-		'''
 		messages = [m[1] for m in self.stack[-8:]]
 		if messages.count(text) >= 5: 
 			text = '...'
 			if messages.count('...') >= 3: 
 				self.sending.release()
 				return
-		'''
 
 		self.__write(('PRIVMSG', recipient), text)
 		self.stack.append((time.time(), text))

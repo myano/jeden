b9f772789e50987695d3833740c74dabdbc0dbb8
Michael Yanovich <yanovich.1@osu.edu>
2010-08-03 23:14:09 -0400

    Replaced tabs with 4 spaces.

--- modules/startup.py
+++ modules/startup.py
@@ -8,19 +8,19 @@ http://inamidst.com/phenny/
 """
 
 def startup(jenny, input): 
-	if hasattr(jenny.config, 'serverpass'): 
-		jenny.write(('PASS', jenny.config.serverpass))
+    if hasattr(jenny.config, 'serverpass'): 
+        jenny.write(('PASS', jenny.config.serverpass))
 
-	if hasattr(jenny.config, 'password'): 
-		jenny.msg('NickServ', 'IDENTIFY %s' % jenny.config.password)
-		__import__('time').sleep(5)
+    if hasattr(jenny.config, 'password'): 
+        jenny.msg('NickServ', 'IDENTIFY %s' % jenny.config.password)
+        __import__('time').sleep(5)
 
-	# Cf. http://swhack.com/logs/2005-12-05#T19-32-36
-	for channel in jenny.channels: 
-		jenny.write(('JOIN', channel))
+    # Cf. http://swhack.com/logs/2005-12-05#T19-32-36
+    for channel in jenny.channels: 
+        jenny.write(('JOIN', channel))
 startup.rule = r'(.*)'
 startup.event = '251'
 startup.priority = 'low'
 
 if __name__ == '__main__': 
-	print __doc__.strip()
+    print __doc__.strip()

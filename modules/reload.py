f43f3301465088cc9750646dd72fc18490275358
Michael Yanovich <yanovich.1@osu.edu>
2010-08-03 23:13:44 -0400

    Replaced tabs with 4 spaces

--- modules/reload.py
+++ modules/reload.py
@@ -10,37 +10,37 @@ http://inamidst.com/phenny/
 import irc
 
 def f_reload(jenny, input): 
-	"""Reloads a module, for use by admins only.""" 
-	if not input.admin: return
+    """Reloads a module, for use by admins only.""" 
+    if not input.admin: return
 
-	name = input.group(2)
-	if name == jenny.config.owner: 
-		return jenny.reply('What?')
+    name = input.group(2)
+    if name == jenny.config.owner: 
+        return jenny.reply('What?')
 
-	if (not name) or (name == '*'): 
-		jenny.setup()
-		return jenny.reply('done')
+    if (not name) or (name == '*'): 
+        jenny.setup()
+        return jenny.reply('done')
 
-	try: module = getattr(__import__('modules.' + name), name)
-	except ImportError: 
-		module = getattr(__import__('opt.' + name), name)
-	reload(module)
-	if hasattr(module, 'setup'): 
-		module.setup(jenny)
+    try: module = getattr(__import__('modules.' + name), name)
+    except ImportError: 
+        module = getattr(__import__('opt.' + name), name)
+    reload(module)
+    if hasattr(module, 'setup'): 
+        module.setup(jenny)
 
-	if hasattr(module, '__file__'): 
-		import os.path, time
-		mtime = os.path.getmtime(module.__file__)
-		modified = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(mtime))
-	else: modified = 'unknown'
+    if hasattr(module, '__file__'): 
+        import os.path, time
+        mtime = os.path.getmtime(module.__file__)
+        modified = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(mtime))
+    else: modified = 'unknown'
 
-	jenny.register(vars(module))
-	jenny.bind_commands()
+    jenny.register(vars(module))
+    jenny.bind_commands()
 
-	jenny.reply('%r (version: %s)' % (module, modified))
+    jenny.reply('%r (version: %s)' % (module, modified))
 f_reload.name = 'reload'
 f_reload.rule = ('$nick', ['reload'], r'(\S+)?')
 f_reload.priority = 'low'
 
 if __name__ == '__main__': 
-	print __doc__.strip()
+    print __doc__.strip()
